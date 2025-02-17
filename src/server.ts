5c08aea6c1e66ee3f414b2cc97b18ce8e25e4214
Gisele <88586474+giseletoledo@users.noreply.github.com>
2025-02-17 16:33:13 +0000

    adicionando rotas no no server

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
 import express, { Request, Response } from "express";
-
-import { myMiddleware } from "./middlewares/my-middleware";
+import { productsRoutes } from "./routes/products-routes";
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+//uso json
 app.use(express.json());
+
+// Registrar a rota
+app.use("/products", productsRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
